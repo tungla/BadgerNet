67cec7cd5e2d596aaa1991d3192ab7b0a74c92bf
Rosie Brezynski <Rosie@Rosies-MacBook-Air.local>
2017-10-23 19:55:15 -0500

    view for announcements

---
+++ app/controllers/announcement_controller.rb
@@ -0,0 +1,5 @@
+class AnnouncementController < ApplicationController
+    #before_action :authenticate_user
+    def index
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008175628) do
+ActiveRecord::Schema.define(version: 20171023010635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "announcements", force: :cascade do |t|
+    t.string "phone_number"
+    t.string "message"
+    t.datetime "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "roles", force: :cascade do |t|
     t.string "name"
