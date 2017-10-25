b21e4a2d4ec065c7211e0d00bf4cdab7f29e8542
Rosie Brezynski <Rosie@Rosies-MacBook-Air.local>
2017-10-25 12:51:24 -0500

    added send type to announcement

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171024013124) do
+ActiveRecord::Schema.define(version: 20171025174838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171024013124) do
     t.datetime "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sendType"
   end
 
   create_table "roles", force: :cascade do |t|
