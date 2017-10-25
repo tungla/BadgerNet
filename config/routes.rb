7fd87015e98efcc0aefe0dbcdd595771aaae9fcc
Chase Schachenman <chaseshak@gmail.com>
2017-10-25 15:09:53 -0500

    Re-order routes.rb file to be alphabetical and more readable

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
+
 Rails.application.routes.draw do
-  get 'permissions/index'
-
-  post 'permissions/delete'
-
   devise_for :users, skip: :registration
+
   root 'home#index'
 
+  # Home controller routes
   get 'home/index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Permissions routes
+  get 'permissions/index'
+  post 'permissions/delete'
+
 end
