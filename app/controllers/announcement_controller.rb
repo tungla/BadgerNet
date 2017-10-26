ca915dc9726dba25a032ac07fbbacbecedd04245
Rosie Brezynski <Rosie@Rosies-MacBook-Air.local>
2017-10-25 20:13:55 -0500

    text message send helper

--- app/controllers/announcement_controller.rb
+++ app/controllers/announcement_controller.rb
@@ -18,23 +18,6 @@ class AnnouncementController < ApplicationController
     end
   end
 
-  def send_text_message
-    number_to_send_to = '2066180749' # "params[:number_to_send_to]"
-
-    twilio_sid = 'ACc17e1968205992bb82bdb0ba8de37732'
-    twilio_token = '6463ef85f8d7f4765d15df5bef850bda'
-    twilio_phone_number = '2062078212'
-
-    @twilio_client = Twilio::REST::Client.new(twilio_sid, twilio_token)
-
-    @twilio_client.messages.create(
-      :from => "+1#{twilio_phone_number}",
-      :to => number_to_send_to,
-      :body => "This is an message. It gets sent to #{number_to_send_to}"
-    )
-  end
-  helper_method :send_text_message
-
   private
 
   def announcement_params
