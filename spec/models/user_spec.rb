897ff1d03f9577c6e7df746d69549d7f1b965213
Chase Schachenman <chaseshak@gmail.com>
2017-10-31 16:45:12 -0500

    Remove uneccessary roles specs and add user model specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:user)).to be_valid
+  end
+
+  describe 'ActiveModel validations' do
+    it 'requires a first name, last name, and phone number' do
+      expect(build(:user, first_name: '')).not_to be_valid
+      expect(build(:user, last_name: '')).not_to be_valid
+      expect(build(:user, phone: '')).not_to be_valid
+    end
+
+    it 'requires a 10 digit US Phone number' do
+      expect(build(:user, phone: '123')).not_to be_valid
+    end
+  end
 end
