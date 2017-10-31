09c29ecc22a3ff6b3674d9c4fd3dac66015924b5
Chase Schachenman <chaseshak@gmail.com>
2017-10-31 15:26:12 -0500

    Add schedule page integration tests and refactor modals to use hidden class

---
+++ spec/features/schedule_page_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature 'Visiting the schedule page', type: :feature do
+  before do
+    visit 'schedule'
+  end
+  scenario 'it loads the schedule page' do
+    expect(current_path).to eq('/schedule')
+  end
+
+  scenario 'it displays the calendar' do
+    expect(page).to have_css('div.calendar-container')
+  end
+end
+
+RSpec.feature 'Creating a new event', type: :feature do
+  before do
+    visit 'schedule'
+  end
+
+  scenario 'the new event button triggers the new event modal' do
+    click_on 'New Event'
+    expect(page.find('#new-event')).not_to have_css('hidden')
+  end
+end
