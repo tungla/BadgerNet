0ca35b5f4edbacea09962b2ff3f77490eeb84bea
Chase Schachenman <chaseshak@gmail.com>
2017-10-31 17:12:41 -0500

    Remove 'date' column from announcement model (redundant due to 'created_at')

--- db/migrate/20171024013124_create_announcements.rb
+++ db/migrate/20171024013124_create_announcements.rb
@@ -2,7 +2,6 @@ class CreateAnnouncements < ActiveRecord::Migration[5.1]
   def change
     create_table :announcements do |t|
       t.string :content
-      t.timestamp :date
 
       t.timestamps
     end
