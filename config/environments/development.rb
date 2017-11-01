046541b79a4bd58584e73cf6f27c1048e9e31990
jack <j2518966@gmail.com>
2017-11-01 12:51:35 -0500

    improved header

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -53,4 +53,24 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings ={
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "example.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: 'badgernet.announcement',
+  password: 'badger1234',
+  :enable_starttls_auto => true
+}
+
+config.active_support.deprecation = :log
+  config.active_record.migration_error = :page_load
+  config.assets.debug = true
+  config.assets.digest = true
+  config.assets.raise_runtime_errors = true
+  config.action_mailer.default_url_options = { host: "localHost:3000"}
 end
