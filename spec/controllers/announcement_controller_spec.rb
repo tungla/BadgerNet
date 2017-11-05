f3c65054a06604216ce8fd549b4df125e76408d7
Rosie Brezynski <Rosie@Rosies-MacBook-Air.local>
2017-11-05 16:38:08 -0600

    tests

--- spec/controllers/announcement_controller_spec.rb
+++ spec/controllers/announcement_controller_spec.rb
@@ -101,7 +101,9 @@ RSpec.describe AnnouncementController, type: :controller do
       end
 
       it 'should remove the announcement' do
-        expect { delete :destroy, params: { id: a.id } }.to change { Announcement.count }.by(-1)
+        expect {
+          delete :destroy, params: { id: a.id }
+        }.to change { Announcement.count }.by(-1)
       end
     end
 
