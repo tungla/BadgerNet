f00ac37e1b1725cadb3d283ff00086f91a0b648e
George Black <georgemblack@users.noreply.github.com>
2017-11-11 16:06:56 -0600

    Create schedule for user if one doesn't exist

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -1,6 +1,15 @@
 # ScheduleController controls adding/editing/viewing schedules
 class ScheduleController < ApplicationController
   def index
-    @schedule = current_user.schedule
+
+    # get current user
+    @user = current_user
+
+    # create schedule if one doesn't exist
+    if @user.schedule
+      @schedule = @user.schedule
+    else
+      @schedule = Schedule.create(user_id: @user.id)
+    end
   end
 end
