84d606522ec4e466e74aae3fad2838f3ccfb2caa
George Black <georgemblack@users.noreply.github.com>
2017-11-13 13:33:44 -0600

    Begin displaying events on calendar

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,4 +2,12 @@
 class Schedule < ApplicationRecord
   belongs_to :user
   has_many :event
+
+  # Return all events on a given day
+  # 0 is Sunday, 1 is Monday, etc.
+  def events_on_day(day_num)
+    events = []
+    event.each { |e| events << e if e.days.include? day_num }
+    events
+  end
 end
