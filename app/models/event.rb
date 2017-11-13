751537f83a5d1d48a5f84c3fa7d77c7bb410dbc0
George Black <georgemblack@users.noreply.github.com>
2017-11-13 12:08:19 -0600

    Display event days as formatted string

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,13 @@
 class Event < ApplicationRecord
   belongs_to :schedule
   validates :name, :start, :end, presence: true
+
+  # Returns a string of the days in which this event repeats
+  # For example: "Mon, Tue, Sat"
+  def days_as_string
+    day_string = ""
+    day_map = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+    self.days.each {|d| day_string << day_map[d] + ", "}
+    return day_string[0..(day_string.length - 3)]
+  end
 end
