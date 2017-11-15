43a963b367e9c6cbba03674502de48ad7eca4b86
pfcarey917 <longspeak917@gmail.com>
2017-11-15 12:45:11 -0600

    Uploading Documents Feature

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -17,4 +17,13 @@ class DocumentsController < ApplicationController
   def message_params
     params.require(:document).permit(:title, :content)
   end
+
+  def upload
+    @person=User.all
+    uploaded_io = params[:Document]
+    File.open(Rails.root.join('public',uploaded_io.original_filename), 'wb') do |file|
+    file.write(uploaded_io.read)
+    end
+  end
+
 end
