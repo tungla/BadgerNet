8dad337889aad1ec4a74a4fb53f42e3e6541697e
pfcarey917 <longspeak917@gmail.com>
2017-11-18 20:49:03 -0600

    Merge branch 'home' of https://github.com/Chaseshak/BadgerNet into home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     if current_user.has_role? :coach
       render 'admin_index'
     else
-      render
+      render 'admin_index'
     end
   end
 end
