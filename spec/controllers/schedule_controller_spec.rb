457508779709eae233d11e514d186a4b45c0e917
George Black <georgemblack@users.noreply.github.com>
2017-11-22 13:08:27 -0600

    Rspec test for create_event

--- spec/controllers/schedule_controller_spec.rb
+++ spec/controllers/schedule_controller_spec.rb
@@ -17,4 +17,19 @@ RSpec.describe ScheduleController, type: :controller do
       include_examples 'renders the template', :index
     end
   end
+
+  describe 'POST #create_event' do
+    before do
+      coach = create(:coach_user)
+      sign_in(coach)
+      get :index
+    end
+    context 'given a valid event' do
+      it 'creates an event' do
+        expect do
+          post :create_event, params: { name: 'My Event', time: {'start'=>'09:00', 'end'=>'16:45'}, days: [0, 2, 3]}
+        end.to change { Event.count }.by(1)
+      end
+    end
+  end
 end
