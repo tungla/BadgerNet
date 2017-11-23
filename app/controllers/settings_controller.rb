468593440a99c0099fa7c9ff20ad8a8ce40442a3
MJanick <mjanick@wisc.edu>
2017-11-22 20:30:07 -0600

    Unresolved master merging changes with stashed changes

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,5 +1,26 @@
+# Settings Controller
 class SettingsController < ApplicationController
   def index
     @user = current_user
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'User Profile Sucessfully Updated'
+    else
+      begin
+        @user.validate!
+      rescue ActiveRecord::RecordInvalid => exception
+        flash[:danger] = exception.message
+      end
+    end
+    redirect_to action: 'index'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone)
+  end
+
 end
