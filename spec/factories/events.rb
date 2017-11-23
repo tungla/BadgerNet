a9d9efaa771b049598ee75d0a2ab4e3e585b1d47
George Black <georgemblack@users.noreply.github.com>
2017-11-22 20:58:07 -0600

    Rspec is frustrating
    Just venting, that's all.

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :event do
-    name 'MyString'
+    name 'My Event'
+    start_time '09:30:00'
+    end_time '12:15:00'
+    days [0, 2, 3]
     schedule nil
   end
 end
