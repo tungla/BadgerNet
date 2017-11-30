ede765bfc1ea8861fbff8b3be38c06df8a297cd3
Chase Schachenman <chaseshak@gmail.com>
2017-11-30 14:33:48 -0600

    Implement pending scope model test

--- spec/models/scope_spec.rb
+++ spec/models/scope_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Scope, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:scope)).to be_valid
+  end
+
+  let(:scope) { build(:scope) }
+
+  describe 'ActiveRecord associations' do
+    it { expect(scope.role).to be_valid }
+  end
 end
