8da8f6f5ac32d72f91c5d3480c5b2c9002b14e0e
Chase Schachenman <chaseshak@gmail.com>
2017-11-30 15:12:36 -0600

    Setup schedule/event associations and test schedule model

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Schedule, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(build(:schedule)).to be_valid
+  end
+
+  let(:schedule) { create(:schedule) }
+
+  describe 'ActiveRecord Associations' do
+    it { expect(schedule.user).to be_valid }
+    it { expect(schedule.event.first).to be_valid }
+    it { expect(schedule.event.count).to eq(Event.count) }
+  end
+
+  describe 'public instance methods' do
+    context 'responds to its methods' do
+      it { expect(schedule).to respond_to(:events_on_day) }
+    end
+
+    context 'executes methods correctly' do
+      context 'events_on_day' do
+        it 'returns the correct events on a day for the schedule' do
+          schedule.event.each do |event|
+            event.days.each do |day_num|
+              expect(schedule.events_on_day(day_num)).to include(event)
+            end
+          end
+        end
+      end
+    end
+  end
 end
