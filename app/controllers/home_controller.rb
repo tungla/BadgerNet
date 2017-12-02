7095b61e136c65ff55f5b41d5e5a10313c979488
pfcarey917 <longspeak917@gmail.com>
2017-12-02 14:22:51 -0600

    Documents block, capitalizing, buttons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@
 class HomeController < ApplicationController
   def index
     @announcements = Announcement.scoped(current_user, limit: 5)
+    @documents = Document.scoped(current_user, limit: 5)
     if current_user.has_role? :coach
       render 'admin_index'
     else
